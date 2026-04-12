7fe3f1005373babcf1e63b1b82ec10eac6c796e8
Nicolás Taborga <n.taborga2003@gmail.com>
2026-04-12 12:25:18 -0400

    Commit3

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: "mainline.proxy.rlwy.net",
-  user: "root",
-  password: "GbkpNUwkfIgdmhDhYIaFGteoHkkBealy",
-  database: "railway",
-  port: 25325
+const db = mysql.createPool({
+  host: process.env.DB_HOST || "mainline.proxy.rlwy.net",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASS || "GbkpNUwkfIgdmhDhYIaFGteoHkkBealy",
+  database: process.env.DB_NAME || "railway",
+  port: process.env.DB_PORT || 25325,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("❌ Error conectando a DB:", err);
-  } else {
-    console.log("🔥 Conectado a Railway MySQL");
-  }D
+// manejar errores globales
+db.on("error", (err) => {
+  console.error("❌ Error en DB:", err);
 });
 
 module.exports = db;
